5efcfa924da5b6679885d7c5de641c017bea8aca
Haitifan03 <samuelwgross2@gmail.com>
2022-10-26 23:44:36 -0400

    Coded test for Blur

--- src/test/java/edu/bsu/cs222/BlurTest.java
+++ src/test/java/edu/bsu/cs222/BlurTest.java
@@ -1,20 +1,48 @@
 package edu.bsu.cs222;
 
 import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
+import javafx.scene.paint.Color;
+
+import static java.lang.Math.abs;
 
 public class BlurTest {
-    String envRootDir = System.getProperty("user.dir");
-    Image testImage = new Image(new FileInputStream("/src/resources/test.png"));
 
-    public BlurTest() throws FileNotFoundException {
+    @Test
+    public void checkSameSize() throws FileNotFoundException {
+        Image testImage = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/resources/test.png"));
+        Image blurredImage = new Blur().blur(testImage, 3);
+
+        Assertions.assertEquals(testImage.getWidth(), blurredImage.getWidth());
+        Assertions.assertEquals(testImage.getHeight(), blurredImage.getHeight());
     }
 
     @Test
-    public void sameSize(){
+    public void averageRedColorDifferenceDecreases() throws FileNotFoundException {
+        Image testImage = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/resources/test.png"));
+        PixelReader pixels = testImage.getPixelReader();
+        Image blurredImage = new Blur().blur(testImage, 3);
+        PixelReader blurredPixels = blurredImage.getPixelReader();
 
+        int differenceSumTest = 0;
+        int differenceSumBlur = 0;
+        for (int x = 0; x < testImage.getWidth() - 2; x++) {
+            for (int y = 0; y < testImage.getHeight(); y++) {
+                Color sample1 = pixels.getColor(x, y);
+                Color sample2 = pixels.getColor(x + 1, y);
+                differenceSumTest += abs((int)(sample1.getRed() * 10.0) - (int)(sample2.getRed() * 10.0));
+                Color sample1Blur = blurredPixels.getColor(x, y);
+                Color sample2Blur = blurredPixels.getColor(x + 1, y);
+                differenceSumBlur += abs((int)(sample1Blur.getRed() * 10.0) - (int)(sample2Blur.getRed() * 10.0));
+
+            }
+        }
+
+        Assertions.assertTrue(differenceSumTest > differenceSumBlur);
     }
 }
