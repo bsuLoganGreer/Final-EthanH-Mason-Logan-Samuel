cdd74f6ad1378e9fcb6b4486163eeb9eee86d5f1
hooperethan123 <emhooper@bsu.edu>
2022-10-26 22:12:58 -0400

    Added test package and cleaned up expand class

--- src/main/java/edu/bsu/cs222/Expand.java
+++ src/main/java/edu/bsu/cs222/Expand.java
@@ -5,42 +5,8 @@ import javafx.scene.image.PixelWriter;
 import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 
-import javax.imageio.ImageIO;
-import java.awt.*;
-import java.awt.image.BufferedImage;
-import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
-
 public class Expand {
-    public static void main(String args[]) throws IOException{
 
-        Scanner sc = new Scanner (System.in);
-        int width ,height;
-        System.out.println("Enter Height and width of image");
-        File input = new File("C:\\Users\\jayfe\\cs224_labs\\Test.jpg");
-        BufferedImage image = ImageIO.read(input);
-
-        height = image.getHeight() * 2;
-        width = image.getWidth() * 2;
-        BufferedImage resized = resizeImage(image, width,height);
-
-        File output = new File("C:\\Users\\jayfe\\cs224_labs\\Test5.png");
-        ImageIO.write(resized, "png", output);
-        System.out.println("Resize Successful");
-        System.out.println("Image size" + height);
-    }
-
-    public static BufferedImage resizeImage(BufferedImage img, int width, int height) {
-
-        Image tmp = img.getScaledInstance(width, height, Image.SCALE_SMOOTH);
-        BufferedImage resized = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
-        Graphics2D g2d = resized.createGraphics();
-        g2d.drawImage(tmp, 0, 0, null);
-        g2d.dispose();
-        return resized;
-
-    }
     public javafx.scene.image.Image resizeImage(javafx.scene.image.Image img, int resizeFactor){
         WritableImage tmp = new WritableImage((int)img.getWidth()*resizeFactor, (int)img.getHeight()*resizeFactor);
         PixelReader reader = img.getPixelReader();
