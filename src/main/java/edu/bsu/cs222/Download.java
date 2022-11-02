fd50547e2f449105052bfa00e1e83f91b1a3748b
Mason Leffel <mjleffel@bsU.edu>
2022-11-02 16:48:00 -0400

    Got download to function, but it fails the tests

--- src/main/java/edu/bsu/cs222/Download.java
+++ src/main/java/edu/bsu/cs222/Download.java
@@ -4,14 +4,13 @@ import javafx.embed.swing.SwingFXUtils;
 import javafx.scene.image.Image;
 
 import javax.imageio.ImageIO;
-import java.awt.image.RenderedImage;
 import java.io.File;
 import java.io.IOException;
 
 public class Download {
     public void download(Image displayImage, String fileName) throws IOException {
 
-        ImageIO.write((RenderedImage) SwingFXUtils.fromFXImage(displayImage, null), "png", new File(System.getProperty("user.dir") +
+        ImageIO.write(SwingFXUtils.fromFXImage(displayImage, null), "png", new File(System.getProperty("user.dir") +
                 "/src/resources/" + "modified_" + fileName));
     }
 }
