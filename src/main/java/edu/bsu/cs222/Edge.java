0ac6055b81ba6d357483788ed67b9dec8f746cbf
Mason Leffel <mjleffel@bsU.edu>
2022-11-07 15:15:20 -0500

    Coded tests

--- src/main/java/edu/bsu/cs222/Edge.java
+++ src/main/java/edu/bsu/cs222/Edge.java
@@ -2,6 +2,8 @@ package edu.bsu.cs222;
 
 import javafx.scene.image.Image;
 import javafx.scene.image.PixelReader;
+import javafx.scene.image.PixelWriter;
+import javafx.scene.image.WritableImage;
 import javafx.scene.paint.Color;
 
 import java.io.FileInputStream;
@@ -10,25 +12,28 @@ import java.io.FileNotFoundException;
 import static java.lang.Math.abs;
 
 public class Edge {
-    public Edge() throws FileNotFoundException {
-        Image testImage = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/resources/test.png"));
-        PixelReader pixels = testImage.getPixelReader();
-        Image blurredImage = new Blur().blur(testImage, 3);
-        PixelReader blurredPixels = blurredImage.getPixelReader();
 
-        int differenceSumTest = 0;
-        int differenceSumBlur = 0;
+    public javafx.scene.image.Image edge(javafx.scene.image.Image image) throws FileNotFoundException {
+        Image testImage = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/resources/test.png"));
+        PixelReader reader = testImage.getPixelReader();
+        WritableImage tmp = new WritableImage(image.getPixelReader(), (int) image.getWidth(), (int)image.getHeight());
+        PixelWriter writer = tmp.getPixelWriter();
+
+
+        int original = 0;
         for (int x = 0; x < testImage.getWidth() - 2; x++) {
             for (int y = 0; y < testImage.getHeight(); y++) {
-                Color sample1 = pixels.getColor(x, y);
-                Color sample2 = pixels.getColor(x + 1, y);
-                differenceSumTest += abs((int)(sample1.getRed() * 10.0) - (int)(sample2.getRed() * 10.0));
-                Color sample1Blur = blurredPixels.getColor(x, y);
-                Color sample2Blur = blurredPixels.getColor(x + 1, y);
-                differenceSumBlur += abs((int)(sample1Blur.getRed() * 10.0) - (int)(sample2Blur.getRed() * 10.0));
+                Color sample1 = reader.getColor(x, y);
+                Color sample2 = reader.getColor(x + 1, y);
+                original+= abs((int)(sample1.getRed() * 10.0)- (int)(sample2.getRed() * 10.0));
+                writer.setArgb(x,y,original);
 
             }
         }
+        return tmp;
+    }
 
+    public boolean shouldSetBlack(Color color, Color color1, double v) {
+        return true;
     }
 }
