c57e7c2a4b6c44ef1924ab3487d6cd44ffc49506
Haitifan03 <samuelwgross2@gmail.com>
2022-11-16 09:43:45 -0500

    Started Pixelate

--- src/test/java/edu/bsu/cs222/PixelateTest.java
+++ src/test/java/edu/bsu/cs222/PixelateTest.java
@@ -1,4 +1,21 @@
 package edu.bsu.cs222;
 
+import javafx.scene.image.Image;
+import javafx.scene.image.PixelReader;
+import javafx.scene.paint.Color;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+
 public class PixelateTest {
+    @Test
+    public void checkAverageWhite() throws FileNotFoundException {
+        Image testImage = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/resources/10X10White.png"));
+        PixelReader testReader = testImage.getPixelReader();
+
+        Color Pixel = new Pixelate().getAveragePixels(testReader, 0, 0);
+        Assertions.assertEquals(Pixel, Color.WHITE);
+    }
 }
