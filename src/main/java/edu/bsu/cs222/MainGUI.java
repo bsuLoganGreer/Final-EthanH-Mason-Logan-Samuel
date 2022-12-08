fc7d21f41b2cd3a8482b13d87264b2399e3c5c1a
Logan <lgreer0221@gmail.com>
2022-12-08 16:47:54 -0500

    Changed the GUI to use BorderPane

--- src/main/java/edu/bsu/cs222/MainGUI.java
+++ src/main/java/edu/bsu/cs222/MainGUI.java
@@ -1,43 +1,48 @@
 package edu.bsu.cs222;
 
-import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
 import javafx.scene.control.*;
-import javafx.scene.control.Button;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
-import javafx.scene.layout.Pane;
-import javafx.scene.layout.VBox;
-import javafx.scene.text.Text;
 import javafx.scene.image.ImageView;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
+import javafx.scene.text.Text;
 import javafx.stage.Stage;
+import javafx.scene.layout.VBox;
+import javafx.application.Application;
 
 import java.io.FileNotFoundException;
 import java.io.IOException;
 
-public class MainGUI extends Application {
+public class MainGUI extends Application{
+
     public MainGUI() throws FileNotFoundException {
     }
 
     public static void main(String[] args){
         launch(args);
     }
+
+
+    private final Insets INSETS = new Insets(10, 10, 10, 10);
     private final TextField inputField = new TextField();
-    private final Button displayPhotoButton = new Button("Apply!");
-    private final Button selectPhotoButton = new Button("Select Photo->");
-    private final Button undoButton = new Button("Undo");
-    private final Button downloadButton = new Button("Download");
+    private final Button selectPhotoButton = createButton("Select Photo->");
+    private final Button displayPhotoButton = createButton("Apply!");
+    private final Button undoButton = createButton("Undo");
+    private final Button downloadButton = createButton("Download");
     private final ColorPicker colorPicker = new ColorPicker();
     private final PhotoManager photoManager = new PhotoManager();
     private final ComboBox<String> filterSelector = new ComboBox<>();
     private ImageView imageView;
     Text errorMessage = new Text("");
-    VBox root;
+    BorderPane root;
+
 
     @Override
     public void start(Stage primaryStage) throws FileNotFoundException {
         inputField.setText(new FilePicker().selectFile(primaryStage));
-        configure(primaryStage);
+        buildUI(primaryStage);
         configureSelectPhotoButton();
         configureComboBox();
         configureDisplayPhotoButton();
@@ -45,21 +50,26 @@ public class MainGUI extends Application {
         configureDownloadButton();
     }
 
-    private void configure(Stage stage) throws FileNotFoundException {
-        stage.setTitle("Photo filter");
-        stage.setScene(new Scene(createRoot()));
-        stage.setWidth(1000);
-        stage.setHeight(800);
+    private void createRoot() throws FileNotFoundException {
+        root = new BorderPane();
+        updateImageView();
+        setRoot();
+    }
+    private void buildUI(Stage stage) throws FileNotFoundException {
+
+        createRoot();
+
+        Scene scene = new Scene(root);
+        stage.setTitle("Art Generator");
+        stage.setScene(scene);
+        stage.setMinWidth(1000);
+        stage.setMinHeight(800);
         stage.show();
     }
 
-    private Pane createRoot() throws FileNotFoundException {
-        root = new VBox();
-        updateImageView();
-        setRoot();
-        return root;
-    }
-    private void configureComboBox(){filterSelector.getItems().addAll("Blur", "Enlarge", "Shrink", "Outline", "Draw Edges", "Create B&W Image","Pixelate", "Stylize","Anime", "Reset", "Paint");}
+    private void configureComboBox(){
+        filterSelector.getItems().addAll("Blur", "Enlarge", "Shrink", "Outline", "Draw Edges", "Create B&W Image","Pixelate",
+                "Stylize","Anime", "Paint", "Reset");}
 
     private void configureSelectPhotoButton() {
         selectPhotoButton.setOnAction(event -> {
@@ -118,7 +128,7 @@ public class MainGUI extends Application {
                     photoManager.pixelateImage();
                 }
                 else if (filterSelector.getValue().equals("Outline")){
-                        photoManager.outlineImage();
+                    photoManager.outlineImage();
                 }
                 else if (filterSelector.getValue().equals("Draw Edges")){
                     photoManager.defineEdges();
@@ -150,22 +160,37 @@ public class MainGUI extends Application {
 
     private void setRoot(){
         errorMessage.setText("");
-        root.getChildren().setAll( //
-                new Label("Enter a file name:"),
-                inputField,//
-                selectPhotoButton, //
-                colorPicker,
-                errorMessage, //
-                new Label("Select a modification:"), //
-                filterSelector, //
-                displayPhotoButton, //
-                undoButton,
-                downloadButton,
-                new Label("Modified Photo:") //
-                , imageView
-        );
+        VBox top = createVbox();
+        top.getChildren().setAll(inputField, selectPhotoButton, errorMessage, colorPicker, filterSelector);
+        root.setTop(top);
+
+
+        HBox bottom = new HBox();
+        bottom.setAlignment(Pos.CENTER);
+        bottom.getChildren().setAll(displayPhotoButton, undoButton, downloadButton);
+        root.setBottom(bottom);
+
+        root.setCenter(imageView);
     }
 
+    private Button createButton(String text) {
+        Button button = new Button(text);
+        button.setMaxWidth(Double.MAX_VALUE);
+        button.setMaxHeight(Double.MAX_VALUE);
+        button.setMinWidth(150);
+        BorderPane.setMargin(button, INSETS);
+        BorderPane.setAlignment(button, Pos.CENTER);
+        return button;
+    }
 
+    private VBox createVbox() {
+        VBox vbox = new VBox();
+        vbox.setMaxWidth(Double.MAX_VALUE);
+        vbox.setMaxHeight(Double.MAX_VALUE);
+        vbox.setMinWidth(150);
+        BorderPane.setMargin(vbox, INSETS);
+        BorderPane.setAlignment(vbox, Pos.CENTER);
+        return vbox;
+    }
 
 }
