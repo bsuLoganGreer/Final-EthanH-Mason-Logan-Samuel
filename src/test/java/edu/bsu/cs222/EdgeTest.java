a552b6ac2dfee0b01b3e80d721d5bee727731ea5
Haitifan03 <samuelwgross2@gmail.com>
2022-12-09 01:32:30 -0500

    Cleaned EdgeTest

--- src/test/java/edu/bsu/cs222/EdgeTest.java
+++ src/test/java/edu/bsu/cs222/EdgeTest.java
@@ -1,34 +1,10 @@
 package edu.bsu.cs222;
 
-import javafx.scene.image.Image;
-import javafx.scene.image.PixelReader;
 import javafx.scene.paint.Color;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
 public class EdgeTest {
-    @Test
-    public void checkColorDifference() throws FileNotFoundException {
-        Image testImage = new Image(new FileInputStream(System.getProperty("user.dir") + "/src/resources/test.png"));
-        PixelReader pixels = testImage.getPixelReader();
-        Image edgedImage = new Edge().createOutline(testImage);
-        PixelReader edgedPixels = edgedImage.getPixelReader();
-
-        for (int x = 0; x < testImage.getWidth() - 2; x++) {
-            for (int y = 0; y < testImage.getHeight() - 1; y++) {
-                if (new Edge().shouldSetBlack(pixels.getColor(x, y), pixels.getColor(x + 1, y), 0.15) ||
-                        new Edge().shouldSetBlack(pixels.getColor(x, y), pixels.getColor(x, y+1), 0.15)){
-                    Assertions.assertEquals(Color.BLACK, edgedPixels.getColor(x, y));
-                }
-                else{
-                    Assertions.assertEquals(Color.WHITE, edgedPixels.getColor(x, y));
-                }
-            }
-        }
-    }
 
     @Test
     public void checkSetBlack() {
