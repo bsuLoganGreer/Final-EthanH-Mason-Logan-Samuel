c80b1f91c518832becf2f3703e9acc076dcaabf8
Haitifan03 <samuelwgross2@gmail.com>
2022-12-09 02:00:51 -0500

    Cleaned pixelate from one letter variable names

--- src/main/java/edu/bsu/cs222/Pixelate.java
+++ src/main/java/edu/bsu/cs222/Pixelate.java
@@ -12,24 +12,24 @@ public class Pixelate {
         PixelReader reader = img.getPixelReader();
         WritableImage tmp = new WritableImage(img.getPixelReader(), (int) img.getWidth(), (int)img.getHeight());
         PixelWriter writer = tmp.getPixelWriter();
-        for (int w=0; w < img.getWidth(); w += 10) {
+        for (int width=0; width < img.getWidth(); width += 10) {
             for (int height = 0; height < img.getHeight(); height += 10) {
-                setAverage(writer, w, height, getAveragePixels(reader, w, height));
+                setAverage(writer, width, height, getAveragePixels(reader, width, height));
             }
         }
         return tmp;
     }
-    public Color getAveragePixels(PixelReader reader, int x, int y) {
+    public Color getAveragePixels(PixelReader reader, int baseWidth, int baseHeight) {
         double averageRed = 0.0;
         double averageGreen = 0.0;
         double averageBlue = 0.0;
         int numPixels = 0;
-        for (int width=0; width <= 10; width++) {
-            for (int hight = 0; hight <= 10; hight++) {
+        for (int addedWidth=0; addedWidth <= 10; addedWidth++) {
+            for (int addedHeight = 0; addedHeight <= 10; addedHeight++) {
                 try {
-                    averageRed += reader.getColor(x + width, y + hight).getRed();
-                    averageGreen += reader.getColor(x + width, y + hight).getGreen();
-                    averageBlue += reader.getColor(x + width, y + hight).getBlue();
+                    averageRed += reader.getColor(baseWidth + addedWidth, baseHeight + addedHeight).getRed();
+                    averageGreen += reader.getColor(baseWidth + addedWidth, baseHeight + addedHeight).getGreen();
+                    averageBlue += reader.getColor(baseWidth + addedWidth, baseHeight + addedHeight).getBlue();
                     numPixels++;
                 }
                 catch (IndexOutOfBoundsException ignored){
@@ -43,11 +43,11 @@ public class Pixelate {
         return new Color(averageRed, averageGreen, averageBlue, 1.0);
     }
 
-    public void setAverage(PixelWriter testWriter, int x, int y, Color color) {
-        for (int w=0; w <= 10; w++) {
-            for (int h = 0; h <= 10; h++) {
+    public void setAverage(PixelWriter testWriter, int baseWidth, int baseHeight, Color color) {
+        for (int addedWidth=0; addedWidth <= 10; addedWidth++) {
+            for (int addedHeight = 0; addedHeight <= 10; addedHeight++) {
                 try {
-                    testWriter.setColor(x+w, y+h, color);
+                    testWriter.setColor(baseWidth+addedWidth, baseHeight+addedHeight, color);
                 }
                 catch (IndexOutOfBoundsException ignored){
 
